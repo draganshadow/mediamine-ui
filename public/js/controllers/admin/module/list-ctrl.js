51ffc3dfb439c890092768f8fe77429e1ecc4ce5
draganshadow <gc@netsyos.com>
2014-06-01 00:49:41 +0200

    Add list and install button

--- public/js/controllers/admin/module/list-ctrl.js
+++ public/js/controllers/admin/module/list-ctrl.js
@@ -1,9 +1,19 @@
 define(['./../../index'], function (controllers) {
     'use strict';
     controllers.controller('ModuleList', function ($scope, $location, Restangular) {
-        Restangular.one('module').getList()
+
+        $scope.install = function (module) {
+            console.log(module);
+            module.post('install', {key:module.key})
+                .then(function(result) {
+                    console.log(result);
+                });
+        };
+
+        Restangular.all('module').getList()
             .then(function(result) {
                 $scope.modules = result;
             });
     });
+
 });
